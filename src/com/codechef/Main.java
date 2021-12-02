b71c7816e1eb50316ed4928fc3e9e2b6b607632f
Nitin <nsinghboss02@gmail.com>
2021-12-02 11:46:51 +0530

    Question added

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -4,8 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
         /*
+        Consider a currency system in which there are notes of six denominations,
+        namely, Rs. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100.
+        If the sum of Rs. N is input, write a program to computer smallest number of notes
+        that will combine to give Rs. N.
 
-         */
+        Input
+        The first line contains an integer T, total number of testcases. Then follow T lines,
+        each line contains an integer N.
+
+        Output
+        For each test case, display the smallest number of notes that will combine to give N,
+        in a new line.
+
+        Constraints
+        1 ≤ T ≤ 1000
+        1 ≤ N ≤ 1000000
+        Example
+        Input
+        3
+        1200
+        500
+        242
+
+        Output
+        12
+        5
+        7
+                                                               */
         Scanner sc = new Scanner(System.in);
         int t, n;
         t = sc.nextInt();
@@ -27,4 +53,4 @@ public class Main {
         return 0;
     }
 }
-// this will give the no of smallest no of notes required:
+// this will give the smallest no of notes required:
